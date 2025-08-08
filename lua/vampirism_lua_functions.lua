dfe6fb6ad22a2775c1d59a95f34b54e18b9eb767
ForestDragon-wesnoth <kolya.vlasenko@gmail.com>
2025-08-08 17:36:55 +0300

    rebalances and improvements

--- lua/vampirism_lua_functions.lua
+++ lua/vampirism_lua_functions.lua
@@ -24,7 +24,7 @@ function vampirism_attach_unit_status_renderer()
   
         table.insert(s, { "element", {
           text = _"Blood: <span color='#ff0000'>".. (u.variables.vampirism_blood and (tostring(u.variables.vampirism_blood)) or "0").."/".. (u.variables.vampirism_max_blood and (tostring(u.variables.vampirism_max_blood)) or "0").. "</span>\n",
-          tooltip = _"TODO"
+          tooltip = _"Blood is the main currency in the Vampirism mod. The main ways of gaining Blood are: \n1. attacking living enemies with the 'drain' special \n2. ending your turn in a village \n\nHunger rate (how much blood you lose per turn): <span color='#ff0000'>"..(u.variables.vampirism_hunger_rate and (tostring(u.variables.vampirism_hunger_rate)) or "0").."</span>"
         } })
 
         --using XP colors that are somewhat similar to default xp colors
@@ -43,7 +43,7 @@ function vampirism_attach_unit_status_renderer()
 
         table.insert(s, { "element", {
           text = _"Level: <span color='#ff0000'>".. (u.variables.vampirism_level and (tostring(u.variables.vampirism_level)) or "0").. "</span> "..xp_prefix.. (u.variables.vampirism_xp and (tostring(u.variables.vampirism_xp)) or "0").."/".. (u.variables.vampirism_max_xp and (tostring(u.variables.vampirism_max_xp)) or "0").. "</span>\n",
-          tooltip = _"TODO"
+          tooltip = _"Vampire level affects your power as a vampire - it increases your HP/damage/some other stats, and lets you unlock various vampire upgrades. However, higher vampire level also has downsides - you take more damage from sunlight, your arcane/fire resistances get lower, and your Hunger rate is increased (how much Blood you lose per turn)\n\nto level up as a vampire, you need Vampire XP, separate from normal XP. Vampire XP is gained when you gain Blood, spend Blood on Powers, and kill enemies (you also gain more Vampire XP when you use a drain attack to kill an enemy, instead of just killing normally)."
         } })
       end
 
